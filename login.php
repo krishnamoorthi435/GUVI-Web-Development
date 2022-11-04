c11d40ee425288e0c69e859d9820d58c928badc4
Krishnamoorthi M <76642930+krishnamoorthi435@users.noreply.github.com>
2022-11-04 10:37:50 +0530

    Update login.php

--- login.php
+++ login.php
@@ -38,6 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <title>Login</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 <body>
